25c3d25f49092d201ec305dfd3a992fee061f5d1
youssef_ammar <youssef.ammar19@yahoo.com>
2023-02-15 10:29:17 +0100

    Entities_User

---
+++ src/main/java/centralachat/Entity/User/Departement.java
@@ -0,0 +1,16 @@
+package centralachat.Entity.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+@Entity
+public class Departement implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+}
